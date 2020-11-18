0a5932e3ac4d2d74b6585b580a01be1f5272f37d
FedericoGavazzi <56387105+FedericoGavazzi@users.noreply.github.com>
2020-11-18 22:28:05 +0100

    Updated style and created structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from "react";
 import "./styles.css";
 
-export default function App() {
+export function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Calcolatrice</h1>
+      <div>
+        <form>
+          <input type="text" placeholder="0"></input>
+        </form>
+        <br></br>
+        <div className="row">
+          <button className="btn btn-est-sx num">7</button>
+          <button className="btn btn-center num">8</button>
+          <button className="btn btn-center num">9</button>
+          <button className="btn btn-est-dx oper">/</button>
+        </div>
+        <div className="row">
+          <button className="btn btn-est-sx num">4</button>
+          <button className="btn btn-center num">5</button>
+          <button className="btn btn-center num">6</button>
+          <button className="btn btn-est-dx oper">*</button>
+        </div>
+        <div className="row">
+          <button className="btn btn-est-sx num">1</button>
+          <button className="btn btn-center num">2</button>
+          <button className="btn btn-center num">3</button>
+          <button className="btn btn-est-dx oper">-</button>
+        </div>
+        <div className="row">
+          <button className="btn btn-est-sx oper">%</button>
+          <button className="btn btn-center num">0</button>
+          <button className="btn btn-center oper">.</button>
+          <button className="btn btn-est-dx oper">+</button>
+        </div>
+      </div>
     </div>
   );
 }
